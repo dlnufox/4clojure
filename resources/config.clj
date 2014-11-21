687e46f4aaa230dc09cf554467612176744430b7
xiaochaihu <guangyao10086@gmail.com>
2014-11-21 16:16:37 +0800

    my own setting

--- resources/config.clj
+++ resources/config.clj
@@ -1,18 +1,18 @@
 {:wrap-reload false
  :db-host "localhost"
  :db-name "4clojure"
- :db-user nil
- :db-pwd nil
+ :db-user "4clojure"
+ :db-pwd "xiaochaihu"
  :jetty-port 8080
  :host "smtp.googlemail.com"
- :user "team@4clojure.com"
+ :user "guangyao10086@gmail.com"
  :problem-submission true
  :advanced-user-count 50
  :pass ""
- :repo-url "https://github.com/4clojure/4clojure"
- :hosts {;; :static "static.4clojure.com"
-         ;; :dynamic "www.4clojure.com"
-         :redirects ["4clojure.com"]}
+ :repo-url "https://github.com/dlnufox/4clojure"
+ :hosts {;:static "localhost"
+         ;:dynamic "localhost"
+         :redirects ["localhost"]}
  :golfing-active true
  :heartbeat nil ; set to, eg, [1 :hour] for periodic heap information on stdout
  ;; this list is just for bootstrapping - the real DB is authoritative
